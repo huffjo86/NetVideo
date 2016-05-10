511bfc10c71ab601d110bb8dd6a0af27d26ac186
huffjo86 <jah70@uakron.edu>
2016-05-09 20:44:22 -0400

    Added a New Release option into NetVideo.cpp

--- src/NetVideo.cpp
+++ src/NetVideo.cpp
@@ -15,15 +15,18 @@ int main() {
     // Videos
     Video lotr("Lord of the Rings", Video::REGULAR);
     Video hp("Harry Potter", Video::CHILDRENS);
+    Video ca("Captain America", Video::NEW_RELEASE);
 
     // Rentals of these movies
     Rental r1(lotr, 10);
     Rental r2(hp, 5);
+    Rental r3(ca, 1);
 
     // Customer with some rentals
     Customer customer("Fred");
     customer.addRental(r1);
     customer.addRental(r2);
+    Customer.addRental(r3);
 
     // Output customer statement
     std::cout << customer.statement();
